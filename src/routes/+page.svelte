95c309bd4e93563e7759202f35bf66e6ed0d738d
Josh Nicholas <hi@thambi.li>
2023-01-10 11:00:26 +1100

    front update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -27,17 +27,15 @@
 <div class='float pl-20px'>
 <p>Ello.</p><br>
 <!-- , <a href='https://joshnicholas.com/projects'>projects</a> I'm working on -->
-<p>My name is Josh. I love learning. This site mostly serves as a host for what would otherwise be more ephemeral - what I’m <a href='https://joshnicholas.com/words'>reading</a> and <a href='https://joshnicholas.com/scribbles'>scribbling</a> etc.</p><br>
+<p>My name is Josh. I love learning. This site hosts what would otherwise be more ephemeral - what I’m <a href='https://joshnicholas.com/words'>reading</a> and <a href='https://joshnicholas.com/scribbles'>scribbling</a> etc. If you scroll back, you can find <a rel="noreferrer" href='https://joshnicholas.com/scribbles/page/38'>the beginning</a> of my scribble journey.</p><br>
 
-<p>By day I am a data journalist at <a rel="noreferrer" href='https://www.theguardian.com/profile/josh-nicholas' target='_blank'>The Guardian</a>. Before that I covered <a href='https://theconversation.com/speaking-with-law-professor-cass-sunstein-on-why-behavioural-science-is-always-nudging-us-101074' rel="noreferrer" target='_blank'>economics</a>, <a href='https://www.businessinsider.com.au/photos-inside-the-library-of-the-future-2015-12' rel="noreferrer" target='_blank'>technology</a>; and made <a href='https://www.thewire.org.au/about/team/alumni/' rel="noreferrer" target='_blank'>podcasts and radio</a>.</p><br>
+<p>By day I am a data journalist at <a rel="noreferrer" href='https://www.theguardian.com/profile/josh-nicholas' target='_blank'>The Guardian</a>. Before that I covered <a href='https://theconversation.com/speaking-with-law-professor-cass-sunstein-on-why-behavioural-science-is-always-nudging-us-101074' rel="noreferrer" target='_blank'>economics</a> and technology, and made <a href='https://www.thewire.org.au/about/team/alumni/' rel="noreferrer" target='_blank'>podcasts and radio</a>.</p><br>
 
-<p>This site is over a decade old and has gone through many platforms and stylistic iterations. The current version was built with <a rel="noreferrer" href='https://github.com/joshnicholas/sk-blog' target='_blank'>a Sveltekit template</a>. A rapidly changing API could break it at any moment.</p><br>
-  
-<p>Content is also being programmatically pulled from other places. As a result some links, line breaks, quotes etc., may be janky.</p><br>
+<p>This site was built with an earlier version of <a rel="noreferrer" href='https://github.com/joshnicholas/sk-blog' target='_blank'>Sveltekit</a>. An API change could break it any day now. Content is also being programmatically pulled from other places. As a result some links, line breaks, quotes etc., may be janky.</p><br>
 
-<p>I post more regularly on <a rel="noreferrer" href='https://micro.blog/JoshNicholas' target='_blank'>micro.blog</a>. You can follow me on <a href='https://micro.blog/JoshNicholas?remote_follow=1' target='_blank'>mastodon</a>.<br>
+<p>I post more regularly on <a rel="me" href='https://micro.blog/JoshNicholas' target='_blank'>micro.blog</a>. You can follow me on <a href='https://micro.blog/JoshNicholas?remote_follow=1' target='_blank'>mastodon</a>.<br>
 </div>
-    
+
 </div>
 
 <style>
