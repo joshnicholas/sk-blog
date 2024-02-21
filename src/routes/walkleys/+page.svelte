a01393973fcddb110a065c71b1ba4e0962e3d226
Josh Nicholas <hi@thambi.li>
2024-02-21 17:14:05 +1100

    Remove extras

--- src/routes/walkleys/+page.svelte
+++ src/routes/walkleys/+page.svelte
@@ -28,14 +28,16 @@
 <BackToTop />
 <div class = "max-w-4xl mx-auto">
 
+  <p class="pb-5 text-center font-bold tracking-tight" style="color:red">This page is still under construction. Some of the models/examples need to be added to, cleaned up, swapped out or retrained.</p>
+
   <h1
-  class="text-center font-bold tracking-tight text-zinc-800 dark:text-zinc-100">
+  class="pb-5 text-center font-bold tracking-tight text-zinc-800 dark:text-zinc-100">
   Jump to section:
 </h1>
   <ul class="list-inside text-center">
-    <li><a href='#rawdata'>Working with raw data</a></li>
-    <li><a href='#processed'>Connecting with processed data</a></li>
-    <li><a href='#patterns'>Finding patterns</a></li>
+    <li><a href='#rawdata'>Extracting info from text</a></li>
+    <li><a href='#processed'>Making connections between datasets</a></li>
+    <li><a href='#patterns'>Finding a needle in a haystack</a></li>
   </ul>
 
 </div>
@@ -44,12 +46,12 @@
 
 <div class="pt-10 pb-10 container text-center tracking-tight text-zinc-800 dark:text-zinc-100">
 
-<h1 id="rawdata">Working with raw data</h1>
+<h1 id="rawdata">Extracting info from text</h1>
 
 <p>This section is about extracting useful information from raw or unstructured data. </p>
 
 <div  class='section pb-10 pt-5'>
-<h2 class="font-bold">Named entity extraction</h2>
+<!-- <h2 class="font-bold">Named entity extraction</h2> -->
 <p>Enter some text and it will isolate the people, organisations,locations and other entities.</p>
 
 <iframe
@@ -61,7 +63,7 @@
 
 
 
-<h2 class="font-bold pt-20">Table extraction</h2>
+<!-- <h2 class="font-bold pt-20">Table extraction</h2>
 <p>If you upload a screenshot of a table it will generate a json feed.</p>
 <p >I've got a sample table <a style="color:#84cc16" href='/budgetExample.png'>here</a> from the Australian federal budget. The code runs cell by cell so it may take a bit of time (especially on larger tables).</p>
 
@@ -71,7 +73,7 @@
 	frameborder="0"
 	width="100%"
 	height="500"
-></iframe>
+></iframe> -->
 
 
 </div>
@@ -82,10 +84,10 @@
 <!-- ### Combining/merging  -->
 
 
-  <h1 id="processed">Connecting with processed data</h1>
+  <h1 id="processed">Making connections between datasets</h1>
   
   <div  class='section pb-10 pt-5'>
-  <h2 class="font-bold">Fuzzy matching</h2>
+  <!-- <h2 class="font-bold">Fuzzy matching</h2> -->
 
 
 <p>Enter some text and it will isolate the people, organisations and locations.</p>
@@ -97,7 +99,7 @@ width="100%"
 height="450"
 ></iframe>
 
-<h2 class="font-bold pt-20">Query a table</h2>
+<!-- <h2 class="font-bold pt-20">Query a table</h2>
 
 <p class="text-center tracking-tight text-zinc-800 dark:text-zinc-100">If you upload a CSV of a table below you can then ask questions in natural language.</p>
 <p class="pt-5 pb-5 text-center tracking-tight">I've got a sample csv <a style="color:#84cc16" href='/boxscore.csv'>here</a> you can play with. It's a cut down box score from a Boston Celtics game. You could ask questions like "What was the average score for the players" or "How many minutes did Jaylen Brown play?"</p>
@@ -109,7 +111,7 @@ height="450"
 	frameborder="0"
 	width="100%"
 	height="700px"
-></iframe>
+></iframe> -->
 
 
 </div>
@@ -118,10 +120,10 @@ height="450"
 
 <!-- ### Categorisation  -->
 
-<h1 id="patterns">Finding patterns</h1>
+<h1 id="patterns">Finding a needle in a haystack</h1>
   
 <div  class='section pb-10 pt-5 max-w-4xl mx-auto text-center tracking-tight text-zinc-800 dark:text-zinc-100'>
-<h2 class="font-bold">Classification</h2>
+<!-- <h2 class="font-bold">Classification</h2> -->
 
 
 <p class="pb-5" >Let's say you have a thousands of articles that you need to put into categories, or prioritise. You could read through them all, or you could use a model to sort through them for you. We had a similar problem on a project at <a style="color:#84cc16" href='https://www.theguardian.com/australia-news/2023/sep/01/coalition-no-campaigners-spending-four-times-more-on-voice-facebook-ads-than-counterparts'>The Guardian</a> - we had thousands of Google ads that we needed to filter to find the ones about the Voice to Parliament.</p>
